385a77959a9056d31cb1015f2ac74ec36ef697a6
Pulse999 <KeneThembinkosi999@gmail.com>
2026-06-25 18:05:31 +0200

    Fix environment configuration to use HTTP for base URL

--- Weighsoft.ui.v1/app/js/env.js
+++ Weighsoft.ui.v1/app/js/env.js
@@ -2,7 +2,7 @@
   window.__env = window.__end || {};
   //window.__env.base = "http://" + window.location.hostname + "/weighsoft/backend/public";
   //window.__env.base = "https://api.weighsoft.co.za";
-  window.__env.base = `https://${window.location.hostname}:5000`;
+  window.__env.base = `http://${window.location.hostname}:5000`;
   window.__env.scale = `http://henzard-pi:3000`;
   window.__env.logo = "assets/images/logos/ZZesto.png";
 })(this);
